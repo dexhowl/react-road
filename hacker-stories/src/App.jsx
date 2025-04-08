d35727b970df45b3e72ee909d2eef5ad6b80e6b7
Dexter Taylor <dtaylor13@live.com>
2025-04-07 22:37:04 -0400

    swap in title varialbe instead of hardcoding

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react"
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg"
 import "./App.css"
 
-function App() {
-  const [count, setCount] = useState(0)
+const title = "Hacker Stories";
 
+function App() {
   return (
    <div>
-     <h1>Hello React</h1>
+     <h1>Hello {title}</h1>
+     <div>
+      <form action="" method="post">
+        <label htmlFor="text">Label </label>
+        <input type="text" name="text" id="text" />
+      </form>
+     </div>
    </div>
   )
 }
