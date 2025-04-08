53678f8cb62dbf271516d669ce4f31718e6555e2
Dexter Taylor <dtaylor13@live.com>
2025-04-07 22:42:57 -0400

    define title and greeting inside the welcome object for display

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react"
 import "./App.css"
 
-const title = "Hacker Stories";
+const welcome = {
+  title: "Hacker Stories",
+  greeting: "Hi"
+}
+
 
 function App() {
+
   return (
    <div>
-     <h1>Hello {title}</h1>
+     <h1>{welcome.greeting} {welcome.title}</h1>
      <div>
       <form action="" method="post">
         <label htmlFor="text">Label </label>
