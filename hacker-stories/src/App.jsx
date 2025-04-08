d43d99662fd891292b8732b8f109ae9b22d237b9
Dexter Taylor <dtaylor13@live.com>
2025-04-07 23:39:54 -0400

    more advanced list rendering

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,16 +1,35 @@
 import "./App.css"
 
-const carArray = [ "Toyota", "Honda", "Ford", "Chevrolet", "Nissan" ];
+const list = [
+  {
+    make: 'Honda',
+    model: 'Civic',
+    year: '2022',
+    miles: '19,240' 
+  },
+  {
+    make: 'Toyota',
+    model: 'Camary',
+    year: '2022',
+    miles: '21,530'
+  }
+];
 
 
 function App() {
 
   return (
    <div>
-     <ul>
-      {carArray.map((value,key) => <li key={key}>{value}</li>)}
-     </ul>
-     
+      {list.map((value,key) => (
+        <div key={key}>
+          <p>{value.make}</p>
+          <ul>
+            <li>Model: {value.model}</li>
+            <li>Year: {value.year}</li>
+            <li>Miles: {value.miles}</li>
+          </ul>
+        </div>
+        ))} 
    </div>
   )
 }
