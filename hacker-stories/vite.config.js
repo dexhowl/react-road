1bb9be8c9d00db926c0c8fd69f475dcf85103ec5
Dexter Taylor <dtaylor13@live.com>
2025-04-10 01:02:53 -0400

    add vitest for future use

--- hacker-stories/vite.config.js
+++ hacker-stories/vite.config.js
@@ -4,4 +4,15 @@ import react from "@vitejs/plugin-react"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    browser:{
+      enabled: true,
+      provider: 'playwright',
+      instances: [
+        { browser: 'firefox'}
+      ]
+    }
+    
+  }
 })
+ 
