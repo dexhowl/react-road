e3801945ea87d556dc040c636052191aa8948c74
Dexter Taylor <dtaylor13@live.com>
2025-04-11 23:26:19 -0400

    update search component with incorrect solution to updating in real time

--- hacker-stories/src/components/Search.jsx
+++ hacker-stories/src/components/Search.jsx
@@ -1,16 +1,18 @@
 
 
 export default function Search() {
+  let query = ''
 
     function handleChange(event) {
-        console.log(event);
-        console.log(event.target.value);
+        //console.log("Change Trigger");
+        query = event.target.value;
     }
     
     return (
       <>
         <label htmlFor="search">Search </label>
         <input type="text" name="search" onChange={handleChange} id="search" />
+        <p>{query}</p>
       </>
     )
   }
