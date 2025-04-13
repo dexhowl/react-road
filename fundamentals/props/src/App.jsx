69ab394323defab5b531674f1398ca42c8e609ff
Dexter Taylor <dtaylor13@live.com>
2025-04-12 21:07:16 -0400

    Initial commit props project

---
+++ fundamentals/props/src/App.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+function App() {
+  const greeting = 'Welcome to React'
+
+  return (
+    <div>
+      <h1>{greeting}</h1>
+    </div>
+  )
+}
+
+export default App
